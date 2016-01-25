117e09cd4cbf0743a8601a0af9dc286f68197aef
juan2three <sk8erjuan@gmail.com>
2016-01-25 14:56:57 -0500

    email

--- contact-form-handler.php
+++ contact-form-handler.php
@@ -1,6 +1,6 @@
 <?php 
 $errors = '';
-$myemail = 'yourname@website.com';//<-----Put Your email address here.
+$myemail = 'juan.perez@sundanceusa.com';//<-----Put Your email address here.
 if(empty($_POST['name'])  || 
    empty($_POST['email']) || 
    empty($_POST['message']))
